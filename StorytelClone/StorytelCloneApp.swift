dd1ed82a60846c9d6db193d75f180cd5b543924b
Esma Nur Arslan <96125581+esmanurarslan@users.noreply.github.com>
2025-09-10 01:01:29 +0300

    Initial Commit

---
+++ StorytelClone/StorytelCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorytelCloneApp.swift
+//  StorytelClone
+//
+//  Created by Esma Nur Arslan on 10.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct StorytelCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
